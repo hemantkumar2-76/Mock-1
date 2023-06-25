ad7110d0c28c6c2b33e681e2ab0e5966241efa8f
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2023-06-25 15:37:49 +0530

    Create qs_1.java

---
+++ qs_1.java
@@ -0,0 +1,15 @@
+class qs_1 {
+    public void moveZeroes(int[] nums) {
+        int i = 0; 
+        for (int num:nums){
+            if(num != 0){
+                nums[i] = num;
+                i++;
+            }
+        }
+        while(i<nums.length){
+            nums[i] = 0;
+            i++;
+        }
+    }
+}
